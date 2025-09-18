bd482ecedb2651d2f25dfe9c5952445b64afccf2
Waseeq <waseeqnazir@gmail.com>
2025-09-18 17:21:48 +0500

    Linting

--- electron-dist/main.js
+++ electron-dist/main.js
@@ -1,7 +1,7 @@
 "use strict";
-var _a = require('electron'), app = _a.app, BrowserWindow = _a.BrowserWindow;
-var isDev = require('electron-is-dev');
-var path = require('path');
+var _a = require("electron"), app = _a.app, BrowserWindow = _a.BrowserWindow;
+var isDev = require("electron-is-dev");
+var path = require("path");
 function createWindow() {
     var win = new BrowserWindow({
         width: 800,
@@ -10,21 +10,21 @@ function createWindow() {
             nodeIntegration: false,
             contextIsolation: true,
             enableRemoteModule: false,
-            preload: path.join(__dirname, 'preload.js')
+            preload: path.join(__dirname, "preload.js"),
         },
     });
     var loadURL = isDev
-        ? 'http://localhost:3000'
-        : "file://".concat(path.join(__dirname, '../out/index.html'));
+        ? "http://localhost:3000"
+        : "file://".concat(path.join(__dirname, "../out/index.html"));
     win.loadURL(loadURL);
 }
 app.whenReady().then(createWindow);
-app.on('window-all-closed', function () {
-    if (process.platform !== 'darwin') {
+app.on("window-all-closed", function () {
+    if (process.platform !== "darwin") {
         app.quit();
     }
 });
-app.on('activate', function () {
+app.on("activate", function () {
     if (BrowserWindow.getAllWindows().length === 0) {
         createWindow();
     }
